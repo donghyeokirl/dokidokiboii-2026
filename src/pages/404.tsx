9afce92a2106f278164478c6e3deb4d7c894c939
Lorenzo Lee <donghyeokirl@gmail.com>
2026-09-23 11:40:06 +0800

    Fix 404 Page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,31 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-import { Link } from "gatsby"
-import Layout from "../components/Layout"
-import PageHeader from "../components/PageHeader"
-import SEO from "../components/SEO"
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+import PageHeader from "../components/PageHeader";
+import SEO from "../components/SEO";
 
 const NotFoundPage: React.FC<PageProps> = () => (
   <Layout>
-    <PageHeader title="Page not found" intro="That link doesn't lead anywhere on this site." />
-    <div style={{ textAlign: "center", padding: "var(--space-4) var(--space-3) var(--space-6)" }}>
-      <Link to="/" style={{ color: "var(--pink)" }}>
-        Back to home
+    <PageHeader
+      title="Page not found"
+      intro="That link doesn't lead anywhere on this site."
+    />
+    <div
+      style={{
+        textAlign: "center",
+        padding: "var(--space-4) var(--space-3) var(--space-6)",
+      }}
+    >
+      <Link to="/" style={{ color: "var(--text-muted)" }}>
+        Back to Home
       </Link>
     </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <SEO title="Page not found" pathname="/404" index={false} />
+export const Head: HeadFC = () => (
+  <SEO title="Page not found" pathname="/404" index={false} />
+);
